d053f2c25028bbe2ec171e9fd9b7eb1c34ce241d
Shamim Sanisales <shamims12@yahoo.com>
2022-04-03 20:48:00 +0430

    mad libs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python-Projects-for-Beginners
 Python Projects for Beginners
+
+# 1- Mad Libs
+The objective of this project is to develop a Mad Libs Generator python project. In this project, the user first has to pick a story by the title of the story. Then the user has to enter specific words like a noun, adverb, verb, food, adjective, etc, according to the requirements. And then the story will be generated.
---
+++ mad_libs.py
@@ -0,0 +1,31 @@
+def main():
+    name1 = input("Enter a name: ")
+    name2 = input("Enter another name: ")
+    activity = input("Enter an activity: ")
+    activity2 = input("Enter another activity: ")
+    place = input("Enter a place: ")
+    thing = input("Enter a thing: ")
+    adjective = input("Enter an adjective: ")
+    animal = input("Enter an animal: ")
+    action = input("Enter an action: ")
+    action2 = input("Enter another action2: ")
+    town  = input("Enter a town : ")
+
+    print(f'''There was once a boy named {name1}. Everyone acknowledged him for his mischievous behaviours.
+    {name1} was always up to something. Whether it be {activity} or {activity2}, the whole village knew it would be amazing.
+    One day {name1} went to {place}. It's there that he saw an {adjective} {animal}. The {animal} was hurt and {name1} wanted to help it.
+    {name1} gave it a {thing} and the {animal} almost seemed to smile. 
+    After a period of seconds the {animal} healed and became friends with the one who was taking care of it. 
+    {name1} even taught it some amazing tricks like how to {action} and to {action2}. When they felt the time was right, 
+    {name1} lead the healed {animal} into the village. The name of the {animal} was {name2} and it only responded to {name1}'s voice. 
+    Never being in a village before, {name2} was causing lots of commotion. Chasing villagers and eating from the open markets! 
+    As upset as the villagers were, they weren't surprised. "They're always up to no good," some would say. 
+    "My, what has {name1} done this time," others would laugh. Over by a corner a {town} had to tell {name1} that they could no longer hang around. 
+    "But sometimes you can secretly come," they smiled. The {animal} seemed to understand, and after a big hug went on 
+    its way back into the wilderness. Out of all the mischievous things {name1} had done, 
+    it could never measure up to how much fun they had with the {animal}. Years later when they were all grown up, 
+    {name1} heard that an {animal} had been spotted nearby with two of its babies. {name1} smiled. "It must be {name2}." 
+    ''')
+
+
+main()
